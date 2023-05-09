49467ee390294ac89887e13a7a4512c45e5b6fc3
dennis-at-adition <127397054+dennis-at-adition@users.noreply.github.com>
2023-05-09 08:46:54 +0200

    Update Package_Template.swift to support multiple targets

--- Package_Template.swift
+++ Package_Template.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "%%name%%",
-            targets: ["%%name%%"])
+            name: "AdSDK",
+            targets: ["%%coreLibraryName%%", "%%swiftUILibraryName%%"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on...
@@ -17,15 +17,28 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+	    
+	// to download this library from a github release, you need a working access to the repo
+		// a good way to do this is by adding a private token
+		// and then setting up your .netrc file
+		// we will provide a detailed howto in the future
         .binaryTarget(
-            name: "%%name%%",
-
-			// to download this library from a github release, you need a working access to the repo
-			// a good way to do this is by adding a private token
-			// and then setting up your .netrc file
-			// we will provide a detailed howto in the future
-            url: "%%url%%.zip",
-            checksum: "%%checksum%%"
+            name: "%%coreLibraryName%%",
+            url: "%%coreUrl%%.zip",
+            checksum: "%%coreChecksum%%"
+        ),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+	    
+	// to download this library from a github release, you need a working access to the repo
+		// a good way to do this is by adding a private token
+		// and then setting up your .netrc file
+		// we will provide a detailed howto in the future
+	    
+        .binaryTarget(
+            name: "%%swiftUILibraryName%%",
+            url: "%%swiftUIUrl%%.zip",
+            checksum: "%%swiftUIChecksum%%"
         )
     ]
 
