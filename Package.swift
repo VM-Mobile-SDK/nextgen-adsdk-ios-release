eabff0b2fb8cdb176d2f317f1ae140cede2a88a1
swift package deployer <mail@example.com>
2023-05-23 11:29:48 +0000

    Updated swift package definition

--- Package.swift
+++ Package.swift
@@ -5,44 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "AdSDK",
-    platforms: [
-    	.iOS(.v14_4),
-    ],
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdSDK",
             targets: ["AdSDKCore", "AdSDKSwiftUI"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on...
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-	    
-	// to download this library from a github release, you need a working access to the repo
-		// a good way to do this is by adding a private token
-		// and then setting up your .netrc file
-		// we will provide a detailed howto in the future
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109415095.zip",
-            checksum: "3a52744d4b321c431a89edb2daca1a70aa842620940258da03a9163fd885c7a3"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109420403.zip",
+            checksum: "55500143a17deb0506a3e7bb94d67699502dc39dcb9520780a2f71cd954f4723"
         ),
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-	    
-	// to download this library from a github release, you need a working access to the repo
-		// a good way to do this is by adding a private token
-		// and then setting up your .netrc file
-		// we will provide a detailed howto in the future
-	    
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109415094.zip",
-            checksum: "6ec2526c2d41f6827a2c49cfa30e14057d1c236d48408d872039daa8f6f18b26"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109420404.zip",
+            checksum: "f17083da54ccfdee7e143c40379aa68b713501165771fd312c59ab56a07b7527"
         )
     ]
-
 )
