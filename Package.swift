1b7ce4dc9329892f234625cbd8447a84678cdcb1
Artem Kvasentskyi <67891065+kvasnetskyi@users.noreply.github.com>
2023-06-05 15:44:56 +0300

    Revert 0.9.2

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/111348551.zip",
-            checksum: "616e0ff8a72a469b1c8abcaadb4cc1ce57b267069f065ae22292f7a3cfc92897"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109449102.zip",
+            checksum: "528577e886448c2041684ff0e4fc16ddc8785ff331f50ccbbd96406327170da8"
         ),
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/111348550.zip",
-            checksum: "c04fbe87844b46fadcf6540d5206fe654f14b5ba6dee045b25760eaedbd6b7a1"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/109449103.zip",
+            checksum: "6671ca98aefbac19fdb3422af57040d63082599e9aca21923aaaf80f88f235a3"
         )
     ]
 )
