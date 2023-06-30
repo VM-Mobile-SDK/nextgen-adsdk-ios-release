e54ac6c0d20512e57f4c7e5be2abf1cdeb7d1fdf
swift package deployer <mail@example.com>
2023-06-30 13:12:02 +0000

    Updated swift package definition

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/111467219.zip",
-            checksum: "fd18fd3a218a0b671bc071df29d17a18904c05a8e9ee6ddf8f22ceefbe851343"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/115015832.zip",
+            checksum: "45de08d26c4d7993f5e92503dd320a20a06b6153646c0b503639c13cd8fa6986"
         ),
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/111467218.zip",
-            checksum: "a505deb786c3d606491a549ed222c23d29a3a37cd784f521630619d58b4b4dbd"
+            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/115015833.zip",
+            checksum: "d6b2d2f96d4c66f303ba61b72fd705b9edb5447ec77b7d8107fa444724510662"
         )
     ]
 )
