221bed1b739c0fd31ae7853666b77800a9f12a94
dennis-at-adition <127397054+dennis-at-adition@users.noreply.github.com>
2023-08-02 15:50:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/117513068.zip",
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/0.9.4/AdSDKCore.xcframework.zip",
             checksum: "aba1aae14f563e427197b9408ef6b26a44c15b1e7da7e4de29d2b4660ecbcd95"
         ),
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://api.github.com/repos/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/assets/117513069.zip",
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/0.9.4/AdSDKSwiftUI.xcframework.zip",
             checksum: "3873965174a36567cfcc606a93b6d29c52e35e1fb306977989464db3809a708c"
         )
     ]
