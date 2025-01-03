dfae80685ad343635ba78506f7243b954181c909
mobilesdk <mobilesdk@virtualminds.com>
2025-01-03 11:47:14 +0000

    Updated swift package definition

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -14,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/5.5.1/AdSDKCore-5.5.1.zip",
-            checksum: "8143b91567757416c1bb73f96f2ade1cf4512c2b81123274ab111091c616ff41"
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/0.0.10/AdSDKCore-0.0.10.zip",
+            checksum: ""
         ),
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/5.5.1/AdSDKSwiftUI-5.5.1.zip",
-            checksum: "8d16d8b21c35435244183ba6a982946f80a3bcc23bdf8c3aa5b3e188984900b7"
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/0.0.10/AdSDKSwiftUI-0.0.10.zip",
+            checksum: ""
         )
     ]
 )
