7a8ad5cba3881f7b7f882a0edc3ac05d9cd23550
mobilesdk <mobilesdk@virtualminds.com>
2025-01-20 10:38:28 +0000

    Updated swift package definition

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdSDKCore",
-            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/6.0.0/AdSDKCore-6.0.0.zip",
-            checksum: "c0d29682963682c2601540703fdcd752e1bad7c00697eac93e966ce21c56b47e"
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/6.1.0/AdSDKCore-6.1.0.zip",
+            checksum: "c2cc617074553c4a9048101c6053c1c471ea7314c06d2959527b41756be6e1a6"
         ),
         .binaryTarget(
             name: "AdSDKSwiftUI",
-            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/6.0.0/AdSDKSwiftUI-6.0.0.zip",
-            checksum: "decfc53b4f4e22812c1260f0b7b06c6fe0545364c59209f07ba87dccef1d377e"
+            url: "https://github.com/VM-Mobile-SDK/nextgen-adsdk-ios-release/releases/download/6.1.0/AdSDKSwiftUI-6.1.0.zip",
+            checksum: "0a2dee17c7ae35f39e421e9da69c0cbb923014d91b9bea56cf01ed8f5701e8c2"
         )
     ]
 )
